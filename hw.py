db0265d1beac253c7370258ff5a79b02123fe6e5
GayatriSrivastava <gayatri.srivastava.cg@gmail.com>
2026-08-24 10:14:18 +0530

    hw

---
+++ hw.py
@@ -0,0 +1,15 @@
+your_name="gayatri"
+your_age=17
+your_city="kanpur"
+student_name="Gayatri Srivastava"
+student_rollNO=32644589
+student_batch=2026-30
+marks=96
+print(marks)
+marks=99
+print(marks)
+name,age,city="aayushi",67,"pune"
+x=y=z=0
+student1 = "Rahul"
+student_name = "Rahul"
+classroom="B.Tech"
