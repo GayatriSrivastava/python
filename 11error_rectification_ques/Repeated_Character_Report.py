0bb9600b30a54e635b95a572eaf2d27b9c4b572d
GayatriSrivastava <gayatri.srivastava.cg@gmail.com>
2026-09-22 09:57:09 +0530

    updation

--- 11error_rectification_ques/Repeated_Character_Report.py
+++ 11error_rectification_ques/Repeated_Character_Report.py
@@ -6,20 +6,3 @@
 #3–4 occurrences → "Repeated"
 #More than 4 → "Highly Repeated"
 #ques7
-text = input("Enter a string: ")
-seen = ""
-for ch in text:
-    if ch not in seen:
-        frequency = 0
-        for x in text:
-            if ch == x:
-                frequency += 1
-        if frequency > 1:
-            print(ch, "=", frequency)
-            if frequency == 2:
-                print("Duplicate")
-            elif frequency <= 4:
-                print("Repeated")
-            else:
-                print("Highly Repeated")
-        seen += ch
